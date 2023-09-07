dd8c92c31abbf8189541e1adb5b94c15a2ecf0f4
Joel Wembo <joelotepawembo@gmail.com>
2023-09-08 02:57:59 +0800

    updated template for clean demo

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 npm install
 npm start
 
+# Dashboard template
+
+## Usage
+
+<!-- #default-branch-switch -->
+
+1. Copy the files into your project, or one of the [example projects](https://github.com/mui/material-ui/tree/master/examples).
+2. Make sure your project has the required dependencies: @mui/material, @mui/icons-material, @emotion/styled, @emotion/react, recharts.
+3. Import and use the `Dashboard` component.
+
+## Demo
+
+<!-- #default-branch-switch -->
+
+View the demo at https://mui.com/material-ui/getting-started/templates/dashboard/.
+
+
 # Using docker
 1. DOCKER_SCAN_SUGGEST=false docker build -t reactprodx .
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sample React Template <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className="App">
+     <BrowserRouter>
+       <Routes>
+         <Route element={<Dashboard />} >
+           <Route path="/" element={<Home />} />
+           <Route path="about" element={<About />} />
+           <Route path="contact" element={<Contact />} />
+         </Route>
+       </Routes>
+     </BrowserRouter>
+   </div>
+ )
 }
 
-export default App;
+export default App
